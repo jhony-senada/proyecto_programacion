011f66829903e418cf1321e0742667f5a3996468
jhony-senada <139669202+jhony-senada@users.noreply.github.com>
2024-03-19 21:03:17 -0600

    Add files via upload

---
+++ proyecto_programacion/app/src/main/java/com/proyectoProgramacion/Lista_empleados.kt
@@ -0,0 +1,11 @@
+package com.proyectoProgramacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lista_empleados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_empleados)
+    }
+}
---
+++ proyecto_programacion/app/src/main/java/com/proyectoProgramacion/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.proyectoProgramacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //aqui seria el login (pag1)
+        
+        //si el login es correcto mandar a lista empleados
+    }
+}
