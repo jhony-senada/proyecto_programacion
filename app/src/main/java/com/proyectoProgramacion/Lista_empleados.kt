28493c12995d7f780c3c2554471432af6fc6caae
Jhony-senada <josckrlos@gmail.com>
2024-05-02 16:10:11 -0600

    los intentos de hacer lo queria feli

--- app/src/main/java/com/proyectoProgramacion/Lista_empleados.kt
+++ app/src/main/java/com/proyectoProgramacion/Lista_empleados.kt
@@ -7,5 +7,12 @@ class Lista_empleados : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_empleados)
+        data class Empleado (
+            var Nombre: String,
+            var CURP: String,
+            //var
+        )
+        //var Empleados =
     }
+
 }
--- app/src/main/java/com/proyectoProgramacion/MainActivity.kt
+++ app/src/main/java/com/proyectoProgramacion/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.proyectoProgramacion
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,30 +11,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //aqui seria el login (pag1)
         //si el login es correcto mandar a lista empleados
-        val btlog =findViewById<Button>(R.id.Login)
+        val btlog = findViewById<Button>(R.id.Login)
         val mail = findViewById<EditText>(R.id.Edittxt_Email).text.toString()
         val password = findViewById<EditText>(R.id.Edittxt_Password).text.toString()
         val pin = findViewById<EditText>(R.id.editTextPin).text.toString().toInt()
+
+        btlog.setOnClickListener {
+            login(mail, password, pin)
+        }
+
+
+    }
+    fun login(mail: String, password: String, pin: Int) {
         data class Persona(
             val email: String,
             val password: String,
             val pin: Int
         )
-        var personas: Array<Persona> = arrayOf(
-            Persona("correo@ejemplo.com","pasword123",1234),
-            Persona("otro@ejemplo.com","pass456",5678)
+        val personas: Array<Persona> = arrayOf(
+            Persona("correo@ejemplo.com", "pasword123", 1234),
+            Persona("otro@ejemplo.com", "pass456", 5678)
         )
-        btlog.setOnClickListener {
-        }
-
-    }
-    fun login(mail: String, password: String, pin: Int, personas: Array<Persona>):Boolean{
-        for(i in 0 until personas.size) {
-            val mailBoss =
-                if (mail == mailBoss) {
-
+        for (i in personas.indices) {
+            var mailBoss = personas[i].email
+            var passBoss = personas[i].password
+            var pinBoss = personas[i].pin
+            if (mail == mailBoss) {
+                if (password == passBoss) {
+                    if (pin == pinBoss) {
+                        val liga = Intent(this, Lista_empleados::class.java)
+                        startActivity(liga)
+                    }
                 }
-        }
+            }else{
+                //la warning aqui
+            }
         }
     }
 }
